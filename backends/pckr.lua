2225bbdea88db6545724e9d54e45196ea8bff688
Sirisak Lueangsaksri <1087399+spywhere@users.noreply.github.com>
2023-10-11 15:53:42 +0700

    feat(backend): add pckr.nvim

---
+++ backends/pckr.lua
@@ -0,0 +1,60 @@
+B.pckr = function (ctx)
+  local loaders = {}
+  local M = {
+    name = 'pckr.nvim',
+    pckr_path = vim.fn.stdpath('data') .. '/pckr/pckr.nvim',
+    pckr_url = 'https://github.com/lewis6991/pckr.nvim',
+    context = {
+      install_command = function ()
+        require('pckr').sync()
+      end
+    }
+  }
+
+  M.is_installed = function ()
+    return not not vim.loop.fs_stat(M.pckr_path)
+  end
+
+  M.install = function ()
+    -- git is not found
+    if vim.fn.executable('git') == 0 then
+      return false
+    end
+
+    vim.cmd(string.format(
+      'silent !git clone --filter=blob:none %s %s'
+      , M.pckr_url, M.pckr_path
+    ))
+
+    return true
+  end
+
+  M.lazy = {
+    setup = function (plugin, options)
+      options.cond = function (load_plugin)
+        if not loaders[plugin.identifier] then
+          loaders[plugin.identifier] = load_plugin
+        end
+      end
+    end,
+    load = function (plugin)
+      if loaders[plugin.identifier] then
+        loaders[plugin.identifier]()
+      end
+    end
+  }
+
+  M.pre_setup = function ()
+    vim.opt.rpt:prepend(M.pckr_path)
+
+    return require('pckr').setup(ctx)
+  end
+
+  M.setup = function (name, options)
+    options[1] = name
+    require('pckr').add(options)
+  end
+
+  return M
+end
+B['pckr.nvim'] = B.pckr
